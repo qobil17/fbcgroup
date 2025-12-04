ea2d6219d8e1d80bd34d8c9297793123986b9620
qobil17 <ibrohimovqobilbek5@gmail.com>
2025-12-04 12:23:08 +0900

    feat: create member schema model

--- apps/fbcgroup-api/src/components/member/member.service.ts
+++ apps/fbcgroup-api/src/components/member/member.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class MemberService {}
+export class MemberService {
+	constructor(@InjectModel('Member') private readonly memberModel: Model<null>) {}
+
+	public async signup(): Promise<string> {
+		return 'signup executed!';
+	}
+
+	public async login(): Promise<string> {
+		return 'login executed!';
+	}
+
+	public async updateMember(): Promise<string> {
+		return 'updateMember executed!';
+	}
+
+	public async getMember(): Promise<string> {
+		return 'getMember executed!';
+	}
+}
