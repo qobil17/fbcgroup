08dd6e2015a8c478b6cd29c85feec01a242e6abd
qobil17 <ibrohimovqobilbek5@gmail.com>
2025-12-08 15:25:12 +0900

    feat: develop global GraphQL error handling system

--- apps/fbcgroup-api/src/app.module.ts
+++ apps/fbcgroup-api/src/app.module.ts
@@ -7,6 +7,8 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
+import { error } from 'console';
+import { T } from './libs/types/common';
 
 @Module({
 	imports: [
@@ -16,6 +18,15 @@ import { DatabaseModule } from './database/database.module';
 			playground: true,
 			uploads: false,
 			autoSchemaFile: true,
+			formatError: (error: T) => {
+				const graphQLFormattedError = {
+					code: error?.extensions.code,
+					message:
+						error?.extensions.exception?.response?.message || error?.extensions?.response?.message || error?.message,
+				};
+				console.log('GRAPHQL GLOBAL ERR:', graphQLFormattedError);
+				return graphQLFormattedError;
+			},
 		}),
 		ComponentsModule,
 		DatabaseModule,
